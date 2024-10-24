f2f11f27d355e24c142299cc11944d5c492a6b75
CarloEmi <carlosspereyra.cp@gmail.com>
2024-10-24 11:02:14 -0300

    se agregan los uml a los que se hizo referencia para el desarrollo de los ejercicios

--- Trabajo Prácticos/Trabajo Práctico 2/Escenario - Restaurante/Restaurante/src/App.java
+++ Trabajo Prácticos/Trabajo Práctico 2/Escenario - Restaurante/Restaurante/src/App.java
@@ -1,8 +1,3 @@
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
-
-
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
